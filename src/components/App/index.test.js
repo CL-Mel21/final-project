f73a2e21c44e1918d0a0ce28ad58fb82c57df665
cai-li <kaley.licai@gmail.com>
2022-08-03 19:27:46 +1000

    commit test.js

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from '.';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from ".";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText("Find a Book");
   expect(linkElement).toBeInTheDocument();
 });
